739e2dc68bdb42e18c17ef466ae86ffcdcbb6b0a
Ivor Ćelić <ivorcelic@gmail.com>
2023-12-14 20:29:39 +0100

    Definiranje kodne stranice

--- edunovawp3.sql
+++ edunovawp3.sql
@@ -2,8 +2,8 @@ use master;
 go
 drop database if exists edunovawp3;
 go
-
-create database edunovawp3;
+-- ovo je komentar
+create database edunovawp3 collate Croatian_CI_AS;
 go
 --drop database edunovawp3;
 --use master
@@ -12,8 +12,8 @@ use edunovawp3;
 create table smjerovi(
 sifra int not null primary key identity(1,1),
 naziv varchar(50) not null,
-trajanje int null, --null se ne piše. Ako ne piše not null onda se podrazumjeva
-cijena decimal(18,2), --iako ništa ne piše je null
+trajanje int null, --null se ne piše. Ako ne piše not null onda se podrazumjeva null
+cijena decimal(18,2), -- iako ništa ne piše je null
 vaucer bit
 );
 
@@ -56,14 +56,12 @@ grupa int not null,
 polaznik int not null
 );
 
---kreiranje vanjskih ključeva
-
-alter table grupe add foreign key (smjer) references smjerovi(sifra);	
+-- kreiranje vanjskih ključeva
+alter table grupe add foreign key (smjer) references smjerovi(sifra);
 alter table grupe add foreign key (predavac) references predavaci(sifra);
+
 alter table clanovi add foreign key (grupa) references grupe(sifra);
 alter table clanovi add foreign key (polaznik) references polaznici(sifra);
-
-
 
 
 
