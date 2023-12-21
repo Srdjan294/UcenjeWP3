5d7abd6f3fa9569671f3cc7b470c261c9892a66d
Srđan Filipović <srdjanfilipovic991@gmail.com>
2023-12-21 15:02:51 +0100

    učenje sql, tvrtka

---
+++ tvrtka.sql
@@ -0,0 +1,44 @@
+use master;
+go
+drop database if exists tvrtka;
+go
+
+create database tvrtka collate Croatian_CI_AS;
+go
+
+use tvrtka;
+
+create table zaposlenici(
+
+	sifra int not null primary key identity(1,1),
+	ime varchar(50),
+	prezime varchar(50),
+	datumRodenja datetime,
+	placa decimal(18,2),
+	invalid bit,
+
+);
+
+create table slike(
+
+	sifra int not null primary key identity(1,1),
+	zaposlenik int not null,
+	redniBroj varchar(40),
+	putanja varchar(40)
+
+);
+
+alter table slike add foreign key (zaposlenik) references zaposlenici(sifra)
+
+
+insert into zaposlenici(ime, prezime, datumRodenja, placa, invalid) values
+('Marko', 'Markoviæ', '2002-12-22', 1234.34, 1),
+('Ivan', 'Ivanociæ', '2002-2-23', 1245.34, 0),
+('Josip', 'Jopiæ', '2001-3-4', 1224.34, 1);
+
+insert into slike(zaposlenik, redniBroj, putanja) values
+(1,'1.','lijevo'),
+(2,'2.','desno');
+
+select * from zaposlenici;
+select * from slike;
