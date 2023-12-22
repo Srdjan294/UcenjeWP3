816eb806f3f0cb3be02cbb81dcfa20a0ce2eb2e3
Srđan Filipović <srdjanfilipovic991@gmail.com>
2023-12-22 13:17:05 +0100

    update

---
+++ update.sql
@@ -0,0 +1,17 @@
+﻿
+
+
+select * from smjerovi
+where sifra = 1; 
+
+
+update smjerovi set naziv = 'C# programiranje'
+where sifra = 1;
+
+
+update smjerovi set trajanje = 300, cijena = 2000
+where sifra = 2;
+
+
+update smjerovi set naziv = 'Serviser', trajanje = 120, cijena = 1500
+where sifra = 3; 
