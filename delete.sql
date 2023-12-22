e20d5662f0e0bfc59b0c9cbc67efd3a260ef1528
Srđan Filipović <srdjanfilipovic991@gmail.com>
2023-12-22 14:02:17 +0100

    delete naredba

---
+++ delete.sql
@@ -0,0 +1,12 @@
+--nemogu obrisati smjerove jer su vanjski kljuè u grupi
+delete from smjerovi;
+
+select * from smjerovi;
+
+delete from smjerovi where sifra = 3;
+
+delete from clanovi;
+
+delete from grupe;
+
+delete from smjerovi;
