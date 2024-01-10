a125515d812dc6e533c51fa1792bb8fca60652c6
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-01-10 21:13:16 +0100

    select na jednu tablicu

---
+++ selectjednatablica.sql
@@ -0,0 +1,114 @@
+﻿select * from smjerovi;
+
+
+-- filtriranje kolona između select i from 
+-- * označava sve kolone
+-- naziv kolone - pojedine kolone
+
+select naziv, naziv from smjerovi;
+
+select naziv, cijena, * from smjerovi;
+
+-- koloni se može promijeniti naziv
+
+select naziv as smjer_naziv, cijena from smjerovi;
+
+-- konstanta - uvijek ista vrijednost
+
+select cijena, naziv, 'Osijek' as mjesto, 2 as broj from smjerovi;
+
+-- izraz - funkcija
+
+select naziv, len(naziv) as znakova from smjerovi;
+
+-- najjednostavniji select
+
+select getdate();
+
+-- filtriranje redova
+-- operatori uspoređivanja
+-- =
+
+select ime, prezime from polaznici
+where prezime = 'Tolić';
+
+-- uspoređivanja < > <= >=
+
+select * from polaznici where sifra < 12;
+
+-- ostali operatori
+-- like -> % označava bilo koji znak/ove
+-- svi polaznici koji u svom imenu imaju slovo a
+
+select * from polaznici 
+where ime like '%a%';
+
+-- between
+
+select * from polaznici 
+where sifra between 2 and 5;
+
+-- in 
+
+select * from polaznici 
+where sifra in (2, 7, 9);
+
+-- logički operatori
+-- and (logički i)
+
+select * from polaznici 
+where ime like '%a%' and sifra > 10;
+
+-- not (logičko ne)
+
+select * from polaznici
+where ime not like '%a%'
+
+-- primjeri like
+-- svi polaznici čije ime počinje slovom T
+
+select * from polaznici 
+where ime like 'T%';
+
+-- poaznice - s najmanjom greškom završava na a
+
+select * from polaznici
+where ime like '%a';
+
+-- or (logičko ili)
+
+select *  from polaznici
+where ime like 'm%' or ime like 'l%';
+
+-- slaganje rezultata
+-- order by
+-- po prezimenu uzlazno (A -> Ž)
+
+select *  from polaznici
+where ime like 'm%' or ime like 'l%'
+order by prezime;
+
+-- po prezimenu silazno (Ž -> A)
+
+select *  from polaznici
+where ime like 'm%' or ime like 'l%'
+order by prezime desc;
+
+-- limitiranje podataka
+
+select top 10 * from polaznici;
+
+-- kreiranje nove tablice na osnovu select podataka
+select top 10 * 
+into odabrani from polaznici;
+
+select * from odabrani;
+
+drop table odabrani;
+
+
+
+
+
+
+
